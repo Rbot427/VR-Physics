7bef8e03e6bc98fb9ba69f1b2be674504ce37eeb
Rbot427 <rbot427@gmail.com>
2016-12-28 14:01:31 -0500

    Added a Quick Tutorial
    This tutorial should run on an Android device

---
+++ Tutorials/Assets/DynamicObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicObject : MonoBehaviour {
+	// Use this for initialization
+	private Rigidbody r;
+	private bool isHeld;
+
+	void Start () {
+		if (this.GetComponent<Rigidbody>() == null)
+			Debug.Log ("Must have rigidbody");
+		else
+			this.r = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void pickUp() {
+		this.r.useGravity = false;
+		this.isHeld = true;
+	}
+
+	public void putDown() {
+		this.r.useGravity = true;
+		this.isHeld = false;
+	}
+
+	public void addForce(Vector3 s, ForceMode mode) {
+		this.r.AddForce (s, mode);
+	}
+}
---
+++ Tutorials/Assets/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+	public DynamicObject target;
+	public int strength = 5;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("r")) {
+			//ToDo:  Make it a different way to pick stuff up
+			target.pickUp ();
+		} else
+			target.putDown ();
+		if (Input.GetKey ("j")) {
+			//Debug.Log ("TRYING TO MOVE THIS FING THING");
+			target.addForce(new Vector3(0.1f, 0, 0), ForceMode.VelocityChange);
+		}
+		this.transform.LookAt (target.transform);
+		if (Input.GetKey (KeyCode.UpArrow))
+			this.transform.position += this.transform.up;
+		if (Input.GetKey (KeyCode.DownArrow))
+			this.transform.position -= this.transform.up;
+		if (Input.GetKey (KeyCode.LeftArrow))
+			this.transform.position -= this.transform.right;
+		if (Input.GetKey (KeyCode.RightArrow))
+			this.transform.position += this.transform.right;
+	}
+}
